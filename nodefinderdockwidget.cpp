3ded6f30a79438c94eaa581f9f1f0ce4a56430ae
Filippo Gentile <gentilefilippo01@gmail.com>
2021-04-10 17:42:35 +0200

    NodeFinderDockWidget: set models

--- nodefinderdockwidget.cpp
+++ nodefinderdockwidget.cpp
@@ -1,8 +1,32 @@
 #include "nodefinderdockwidget.h"
 
+#include <QTableView>
+#include <QScrollArea>
+
+#include <QVBoxLayout>
+
 NodeFinderDockWidget::NodeFinderDockWidget(NodeFinderMgr *mgr, QWidget *parent) :
     QWidget(parent),
     nodeMgr(mgr)
 {
 
+    QWidget *scrollAreaContents = new QWidget;
+    QVBoxLayout *scrollLay = new QVBoxLayout(scrollAreaContents);
+
+    labelsView = new QTableView;
+    scrollLay->addWidget(labelsView);
+
+    tracksView = new QTableView;
+    scrollLay->addWidget(tracksView);
+
+    QVBoxLayout *mainLay = new QVBoxLayout(this);
+    QScrollArea *scrollArea = new QScrollArea;
+    scrollArea->setWidget(scrollAreaContents);
+    mainLay->addWidget(scrollArea);
+}
+
+void NodeFinderDockWidget::setModels(QAbstractItemModel *labels, QAbstractItemModel *tracks)
+{
+    labelsView->setModel(labels);
+    tracksView->setModel(tracks);
 }
--- nodefinderdockwidget.h
+++ nodefinderdockwidget.h
@@ -5,14 +5,22 @@
 
 class NodeFinderMgr;
 
+class QTableView;
+class QAbstractItemModel;
+
 class NodeFinderDockWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit NodeFinderDockWidget(NodeFinderMgr *mgr, QWidget *parent = nullptr);
 
+    void setModels(QAbstractItemModel *labels, QAbstractItemModel *tracks);
+
 private:
     NodeFinderMgr *nodeMgr;
+
+    QTableView *labelsView;
+    QTableView *tracksView;
 };
 
 #endif // NODEFINDERDOCKWIDGET_H
