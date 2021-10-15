2ef8794ffd4407bbf045d55ba2fce336cd84c3a6
Filippo Gentile <gentilefilippo01@gmail.com>
2021-10-15 11:22:29 +0200

    Add SSPViewer sources

---
+++ viewer/main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication::setOrganizationName(QLatin1String("Filippo"));
+    QApplication::setApplicationDisplayName(QLatin1String("SVG Station Plan Editor"));
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
