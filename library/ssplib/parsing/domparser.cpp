bac2bfe897710db4335dae08a2b0713c9e2f89b1
Filippo Gentile <gentilefilippo01@gmail.com>
2021-10-15 18:20:35 +0200

    DOMParser: implement doc processing

--- library/ssplib/parsing/domparser.cpp
+++ library/ssplib/parsing/domparser.cpp
@@ -2,22 +2,228 @@
 
 #include "../utils/svg_constants.h"
 
+#include "../stationplan.h"
+
+#include "editinginfo.h"
+
 #include "parsinghelpers.h"
+
+#include <QDebug>
 
 #ifdef SSPLIB_ENABLE_EDITING
 
 using namespace ssplib;
 
-DOMParser::DOMParser(QDomDocument *doc, StationPlan *ptr) :
+DOMParser::DOMParser(QDomDocument *doc, StationPlan *ptr, EditingInfo *info) :
     m_doc(doc),
-    plan(ptr)
+    plan(ptr),
+    m_info(info)
 {
 
 }
 
 bool DOMParser::parse()
 {
+    QDomElement root = m_doc->documentElement();
+    processGroup(root);
     return true;
+}
+
+void DOMParser::processGroup(QDomElement &g)
+{
+    QDomNode n = g.firstChild();
+    while(!n.isNull())
+    {
+        // Try to convert the node to an element.
+        QDomElement e = n.toElement();
+        if(!e.isNull())
+        {
+            // The node really is an element.
+            m_info->storeElement(e);
+
+            if(e.tagName() == ssplib::svg_tags::GroupTag)
+            {
+                //Process also sub elements
+                processGroup(e);
+            }
+            else if(e.tagName() == ssplib::svg_tags::TextTag)
+            {
+                processText(e);
+            }
+            else if(e.tagName() == ssplib::svg_tags::DefsTag)
+            {
+                processDefs(e);
+            }
+        }
+        n = n.nextSibling();
+    }
+}
+
+void DOMParser::processDefs(QDomElement &defs)
+{
+    QDomNode n = defs.firstChild();
+    while(!n.isNull())
+    {
+        QDomElement e = n.toElement(); // try to convert the node to an element.
+        if(!e.isNull())
+        {
+            if(e.tagName() == ssplib::svg_tags::DefsTag)
+            {
+                processDefs(e);
+            }
+            else if(e.tagName() == ssplib::svg_tags::FontTag)
+            {
+                //Add ID if missing
+                if(!e.hasAttribute(ssplib::svg_attr::ID))
+                {
+                    int counter = 0;
+                    const QString newId = m_info->getFreeId_internal(QLatin1String("font_"), counter);
+                    if(newId.isEmpty())
+                        qWarning() << "EMPTY FONT ID";
+                    else
+                        e.setAttribute(ssplib::svg_attr::ID, newId);
+                }
+            }
+        }
+        n = n.nextSibling();
+    }
+}
+
+void DOMParser::processText(QDomElement &text)
+{
+    //xml:space="preserve" moves text to right, remove it
+    text.removeAttribute(ssplib::svg_attr::XmlSpace);
+
+    QDomNode n = text.firstChild();
+    while(!n.isNull())
+    {
+        if(n.isText())
+        {
+            //Skip normal text (leave it as is)
+            n = n.nextSibling();
+            continue;
+        }
+
+        //Try to convert the node to an element.
+        QDomElement e = n.toElement();
+        if(e.isNull())
+        {
+            //Node is not an element, skip it
+            n = n.nextSibling();
+            continue;
+        }
+
+        //The node really is an element.
+        if(e.tagName() == ssplib::svg_tags::TextTag)
+        {
+            //Remove text elements inside a text element
+            qDebug() << "TEXT inside TEXT" << e.lineNumber() << e.columnNumber();
+            QDomNode old = n;
+            n = n.nextSibling();
+            text.removeChild(old);
+        }
+        else
+        {
+            if(e.tagName() == ssplib::svg_tags::TSpanTag)
+            {
+                if(e.hasAttribute(ssplib::svg_attr::ID))
+                {
+                    const QString id = e.attribute(ssplib::svg_attr::ID);
+                    if(m_info->namedElements.contains(id))
+                    {
+                        //Duplicate id, remove ID
+                        e.removeAttribute(ssplib::svg_attr::ID);
+                    }
+                }
+
+                processTspan(e, text);
+            }
+
+            n = n.nextSibling();
+        }
+    }
+}
+
+void DOMParser::processTspan(QDomElement &tspan, QDomElement &text)
+{
+    QString value;
+
+    QDomNode n = tspan.firstChild();
+    while(!n.isNull())
+    {
+        if(n.isText())
+        {
+            //Keep text
+            value.append(n.nodeValue());
+            QDomNode old = n;
+            n = n.nextSibling();
+            tspan.removeChild(old);
+            continue;
+        }
+
+        // Try to convert the node to an element.
+        QDomElement e = n.toElement();
+        if(!e.isNull())
+        {
+            // the node really is an element.
+            if(e.tagName() == ssplib::svg_tags::TSpanTag)
+            {
+                processInternalTspan(tspan, e, value);
+            }
+        }
+        n = n.nextSibling();
+        if(!e.isNull())
+        {
+            //Remove internal tspan
+            tspan.removeChild(e);
+        }
+    }
+
+    for(const QString& attr : ssplib::svg_attr::TSpanPassToTextAttrs)
+    {
+        if(tspan.hasAttribute(attr))
+        {
+            text.setAttribute(attr, tspan.attribute(attr));
+            tspan.removeAttribute(attr);
+        }
+    }
+
+    QDomText textVal = m_doc->createTextNode(value);
+    tspan.appendChild(textVal);
+}
+
+void DOMParser::processInternalTspan(QDomElement &top, QDomElement &cur, QString &value)
+{
+    QDomNode n = cur.firstChild();
+    while(!n.isNull())
+    {
+        if(n.isText())
+        {
+            //Keep text
+            value.append(n.nodeValue());
+            n = n.nextSibling();
+            continue;
+        }
+
+        // Try to convert the node to an element.
+        QDomElement e = n.toElement();
+        if(!e.isNull())
+        {
+            // the node really is an element.
+            if(e.tagName() == ssplib::svg_tags::TSpanTag)
+            {
+                processInternalTspan(top, e, value);
+            }
+        }
+
+        n = n.nextSibling();
+    }
+
+    for(const QString& attr : ssplib::svg_attr::TSpanPassAttrs)
+    {
+        if(cur.hasAttribute(attr))
+            top.setAttribute(attr, cur.attribute(attr));
+    }
 }
 
 #endif // SSPLIB_ENABLE_EDITING
--- library/ssplib/parsing/domparser.h
+++ library/ssplib/parsing/domparser.h
@@ -3,22 +3,33 @@
 
 #ifdef SSPLIB_ENABLE_EDITING
 
-#include "../stationplan.h"
-
 class QDomDocument;
+class QDomElement;
+class QString;
 
 namespace ssplib {
+
+class StationPlan;
+class EditingInfo;
 
 class DOMParser
 {
 public:
-    DOMParser(QDomDocument *doc, StationPlan *ptr);
+    DOMParser(QDomDocument *doc, StationPlan *ptr, EditingInfo *info);
 
     bool parse();
 
 private:
+    void processGroup(QDomElement& g);
+    void processDefs(QDomElement& defs);
+    void processText(QDomElement& text);
+    void processTspan(QDomElement &tspan, QDomElement &text);
+    void processInternalTspan(QDomElement &top, QDomElement &cur, QString &value);
+
+private:
     QDomDocument *m_doc;
     StationPlan *plan;
+    EditingInfo *m_info;
 };
 
 } // namespace ssplib
